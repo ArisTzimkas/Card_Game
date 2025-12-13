a34a6238dc624d2e377055a311be33a140e698b6
ArisTzimkas <152434201+ArisTzimkas@users.noreply.github.com>
2025-12-13 19:06:35 +0200

    changes

--- API/dbconnect.php
+++ API/dbconnect.php
@@ -1,1 +1,20 @@
+<?php
+$host='localhost';
+$db = 'adise25_2021168';
+require_once "db_upass.php";
 
+$user=$DB_USER;
+$pass=$DB_PASS;
+
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/iee/2021/iee2021168/mysql/run/mysql.sock');
+} else {
+		//$pass=null;
+    $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+?>
